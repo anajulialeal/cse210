8544a1f83b1399460030474f937210a35c66199b
anajulialeal <lealanju@gmail.com>
2026-03-06 11:12:55 -0300

    changes

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+         string firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
+
+
     }
 }
+
+
+/*
+
+Perguntar
+↓
+Guardar resposta
+↓
+Perguntar
+↓
+Guardar resposta
+↓
+Mostrar resultado
+
+-----------------------------------
+What is your first name? Ana
+What is your last name? Hilamatu
+
+Your name is Silva, Ana Hilamatu. - "Your name is last-name, first-name, last-name" 
+------------------------------------
+*/
